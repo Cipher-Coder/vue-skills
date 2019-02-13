daa72d0b79e0cb1bdc579555415d72c75a6a6141
Cipher-Coder <matthew.rebehn@gmail.com>
2019-02-13 13:16:06 -0500

    updating a little

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class="about">
-        <h1>
-            Hello {{ their_name }}, this is an About Page
-        </h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod timpor invididunt</p>
-    </div>
+  <div class="about">
+    <h1>Hello {{ their_name }}, this is an About Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod timpor invididunt</p>
+  </div>
 </template>
 
 
 <script>
-   export default {
-    name: 'Skills',
-    data() {
-        return {
-            their_name: this.$route.params.name
-        }
-    }
-}
+export default {
+  name: "Skills",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
 </script>
